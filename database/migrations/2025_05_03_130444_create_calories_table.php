b5e0d0ec5ed4a86710b87bc61cb84c938c65ee64
pugachev <pugachev@gmail.com>
2025-05-03 13:07:25 +0900

    不要ファイル削除

--- database/migrations/2025_05_03_130444_create_calories_table.php
+++ database/migrations/2025_05_03_130444_create_calories_table.php
@@ -4,29 +4,28 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateCaloriesTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('calories', function (Blueprint $table) {
             $table->id();
-            $table->dateTime('tgtdate');
-            $table->integer('tgttimezone');
-            $table->integer('tgtcategory')->comment('カテゴリ');
-            $table->string('tgtitem')->nullable();
-            $table->integer('tgtcalorie');
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('calories');
     }
-}; 
+}
